df5fb74679a335b2f6a832f14aede96a36965aa7
peter <tranpeter1095@gmail.com>
2023-09-06 17:59:58 -0600

    did index.js prompt

---
+++ index.js
@@ -0,0 +1,123 @@
+/**
+ * B-I-N-G-O
+ *
+ * A Bingo card contain 25 squares arranged in a 5x5 grid (five columns
+ * and five rows). Each space in the grid contains a number between 1
+ * and 75. The center space is marked "FREE" and is automatically filled.
+ *
+ * As the game is played, numbers are drawn. If the player's card has
+ * that number, that space on the grid is filled.
+ *
+ * A player wins BINGO by completing a row, column, or diagonal of filled
+ * spaces.
+ *
+ * Your job is to complete the function that takes a bingo card and array
+ * of drawn numbers and return 'true' if that card has achieved a win.
+ *
+ * A bingo card will be 25 element array. With the string 'FREE' as the
+ * center element (index 12). Although developers are unscrupulous, they
+ * will pass valid data to your function.
+ */
+
+function checkForBingo(bingoCard, drawnNumbers) {
+  const winConditions = [
+    [0, 1, 2, 3, 4],
+    [5, 6, 7, 8, 9],
+    [10, 11, 12, 13, 14],
+    [15, 16, 17, 18, 19],
+    [20, 21, 22, 23, 24],
+    [0, 5, 10, 15, 20],
+    [1, 6, 11, 16, 21],
+    [2, 7, 12, 17, 22],
+    [3, 8, 13, 18, 23],
+    [4, 9, 14, 19, 24],
+    [0, 6, 12, 18, 24],
+    [4, 8, 12, 16, 20],
+  ];
+
+  const drawnNumbersSet = new Set(drawnNumbers);
+
+  for (const condition of winConditions) {
+    const hasBingo = condition.every((index) => {
+      if (bingoCard[index] === 'FREE') {
+        return true;
+      }
+      return drawnNumbersSet.has(bingoCard[index]);
+    });
+    if (hasBingo) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+// here are some samples
+
+// this should return true with diagonal + free
+console.log(
+  checkForBingo(
+    [
+      8,
+      29,
+      35,
+      54,
+      65,
+      13,
+      24,
+      44,
+      48,
+      67,
+      9,
+      21,
+      'FREE',
+      59,
+      63,
+      7,
+      19,
+      34,
+      53,
+      61,
+      1,
+      20,
+      33,
+      46,
+      72,
+    ],
+    [8, 24, 53, 72]
+  )
+);
+
+// this should return false
+console.log(
+  checkForBingo(
+    [
+      8,
+      29,
+      35,
+      54,
+      65,
+      13,
+      24,
+      44,
+      48,
+      67,
+      9,
+      21,
+      'FREE',
+      59,
+      63,
+      7,
+      19,
+      34,
+      53,
+      61,
+      1,
+      20,
+      33,
+      46,
+      72,
+    ],
+    [1, 33, 53, 65, 29, 75]
+  )
+);
